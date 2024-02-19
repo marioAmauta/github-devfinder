65bd17e2bb449f93ab4b10ebbc997c475d624a40
Mario <94489415+marioAmauta@users.noreply.github.com>
2024-02-19 20:24:02 -0300

    Remove artificial throttling in searchAction

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -3,8 +3,6 @@
 import { redirect } from "next/navigation";
 
 export async function searchAction(prevState: any, formData: FormData) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-
   if (!formData.get("search")) redirect("/");
 
   redirect(`/?user=${formData.get("search")}`);
