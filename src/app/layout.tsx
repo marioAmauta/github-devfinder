7fc0dbc9c7bdb47baad88d84f4a3244dba9ac42f
mario <sks_894@hotmail.com>
2024-10-04 10:18:20 -0300

    Refactor root layout: Move next theme provider to root layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import "./globals.css";
+
 import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import { Roboto_Mono } from "next/font/google";
-import { Providers } from "./providers";
-import { ReactNode } from "react";
-import { ThemeSwitch } from "./components/ThemeSwitch";
-import Link from "next/link";
+import { PropsWithChildren } from "react";
+
+import { cn } from "@/lib/utils";
+
+import { Header } from "@/components/layout/header";
 
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
@@ -13,27 +16,14 @@ export const metadata: Metadata = {
   description: "A Web App built with Nextjs to find developers on GitHub"
 };
 
-export default function RootLayout({
-  children
-}: Readonly<{
-  children: ReactNode;
-}>) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${robotoMono.className} flex min-h-dynamic-screen flex-col items-center bg-primary-bg-light text-font-color-primary-light  dark:bg-primary-bg-dark dark:text-font-color-primary-dark`}
-      >
-        <Providers>
-          <header className="w-full max-w-screen-lg">
-            <nav className="flex items-center justify-between p-4">
-              <Link href="/" className="text-xl font-bold text-inherit">
-                devfinder
-              </Link>
-              <ThemeSwitch />
-            </nav>
-          </header>
+      <body className={cn(robotoMono.className, "flex min-h-dynamic-screen flex-col items-center")}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Header />
           <main className="w-full max-w-screen-lg flex-grow space-y-6 p-3">{children}</main>
-        </Providers>
+        </ThemeProvider>
       </body>
     </html>
   );
